6562175427121c7b9dc2e41fd4146fd398ec16a3
JakeDean1 <jakedean1996@yahoo.co.uk>
2025-04-30 14:28:19 +0100

    Delete JobDetailsPage.jsx

--- JobDetailsPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function JobDetailsPage() {
-  return (
-    <div>
-      <h1>Job Details Page</h1>
-      <p>This page will show details about each scheduled job.</p>
-    </div>
-  );
-}
