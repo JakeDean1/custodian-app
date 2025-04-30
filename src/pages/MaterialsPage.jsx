1e9d256e1392d93b23383f3db71e08de047eecab
JakeDean1 <jakedean1996@yahoo.co.uk>
2025-04-30 14:27:20 +0100

    Create MaterialsPage.jsx

---
+++ src/pages/MaterialsPage.jsx
@@ -0,0 +1,8 @@
+export default function MaterialsPage() {
+  return (
+    <div>
+      <h1>Materials Page</h1>
+      <p>This is where materials used on jobs will be listed and tracked.</p>
+    </div>
+  );
+}
