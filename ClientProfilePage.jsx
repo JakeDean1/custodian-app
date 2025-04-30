cbc031832158e5e3adc0428a0d6b3580939f0e6d
JakeDean1 <jakedean1996@yahoo.co.uk>
2025-04-30 14:27:54 +0100

    Delete ClientProfilePage.jsx

--- ClientProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-export default function ClientProfilePage() {
-  return (
-    <div>
-      <h1>Client Profile Page</h1>
-      <p>This page will display client details and contact information.</p>
-    </div>
-  );
-}
