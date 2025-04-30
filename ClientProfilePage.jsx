8fbac9bb104cc53dec1cf45c30445df1b3b1a32b
JakeDean1 <jakedean1996@yahoo.co.uk>
2025-04-30 14:13:51 +0100

    Create ClientProfilePage.jsx

---
+++ ClientProfilePage.jsx
@@ -0,0 +1,8 @@
+export default function ClientProfilePage() {
+  return (
+    <div>
+      <h1>Client Profile Page</h1>
+      <p>This page will display client details and contact information.</p>
+    </div>
+  );
+}
