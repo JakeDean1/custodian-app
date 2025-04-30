b2dfe236cbd16bd1741df9b2f4039cc7d1d57bef
JakeDean1 <jakedean1996@yahoo.co.uk>
2025-04-30 14:14:12 +0100

    Create JobDetailsPage.jsx

---
+++ JobDetailsPage.jsx
@@ -0,0 +1,8 @@
+export default function JobDetailsPage() {
+  return (
+    <div>
+      <h1>Job Details Page</h1>
+      <p>This page will show details about each scheduled job.</p>
+    </div>
+  );
+}
