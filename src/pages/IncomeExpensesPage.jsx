0b2b590284bc39ed4c90f62292763e89c8a981f5
JakeDean1 <jakedean1996@yahoo.co.uk>
2025-04-30 14:27:37 +0100

    Create IncomeExpensesPage.jsx

---
+++ src/pages/IncomeExpensesPage.jsx
@@ -0,0 +1,8 @@
+export default function IncomeExpensesPage() {
+  return (
+    <div>
+      <h1>Income & Expenses Page</h1>
+      <p>This page will show total income and expenses for selected periods.</p>
+    </div>
+  );
+}
